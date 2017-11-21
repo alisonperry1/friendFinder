bf551aa2c69395b8dce000ea0f4f683ac9b2167a
alison <alison.faith.perry@gmail.com>
2017-11-21 14:30:56 -0600

    blah

---
+++ app/data/friends.js
@@ -0,0 +1,85 @@
+var friends = [
+  {
+    name: "Pete Wentz",
+    photo: "http://www.trbimg.com/img-56e09ca0/turbine/ct-pete-wentz-school-of-rock-20160309",
+    scores: [
+      "5",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
+  },
+  {
+    name: "Robert Downey Jr.",
+    photo: "http://www.bollywoodlife.com/wp-content/uploads/2016/12/Robert-Downey-Jr-Iron-Man.jpg",
+    scores: [
+      "4",
+      "2",
+      "5",
+      "1",
+      "3",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2"
+    ]
+  },
+  {
+    name: "Lindsey Lohan",
+    photo: "http://akns-images.eonline.com/eol_images/Entire_Site/2016210/rs_300x300-160310135552-600.Lindsay-Lohan-Labor-Pains.ms.031016.jpg",
+    scores: [
+      "5",
+      "2",
+      "2",
+      "2",
+      "4",
+      "1",
+      "3",
+      "2",
+      "5",
+      "5"
+    ]
+  },
+  {
+    name: "Pusha T",
+    photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Pusha_T_2013.jpg/1200px-Pusha_T_2013.jpg",
+    scores: [
+      "3",
+      "3",
+      "4",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    name: "Snoop Dogg",
+    photo: "https://www.biography.com/.image/t_share/MTQ3NjM5ODIyNjU0MTIxMDM0/snoop_dogg_photo_by_estevan_oriol_archive_photos_getty_455616412.jpg",
+    scores: [
+      "4",
+      "4",
+      "2",
+      "3",
+      "2",
+      "2",
+      "3",
+      "2",
+      "4",
+      "5"
+    ]
+  }
+
+];
+
+module.exports = friends;
